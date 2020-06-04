00a6b407006178d53189fb3924d67c376856dd32
Kyle <k.yasumiishi@gmail.com>
2020-06-03 18:28:19 -0700

    Added collapsible class and script

---
+++ script.js
@@ -0,0 +1,12 @@
+const coll = document.getElementsByClassName("collapsible");
+
+for (let i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        let content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+}
